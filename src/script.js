2b408a26a65c4737df97e1f816ced16f62a63ce8
kaducodes <kadu.codes@gmail.com>
2024-10-29 22:58:46 -0300

    atualizações com css e javascript
    - rolagem quando clica nos itens do head
    - 'mãozinha' quando passa o mouse
    - ajustes no tamanho das sections

--- src/script.js
+++ src/script.js
@@ -1,1 +1,31 @@
-const head = document.getElementById("head")
+//rolagem para inicio
+document.getElementById("link-to-inicio").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("inicio").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
+
+//rolagem para serviços
+document.getElementById("link-to-servicos").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("servicos").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
+
+//rolagem para contato
+document.getElementById("link-to-contato").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("contato").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
+
+//rolagem para sobre
+document.getElementById("link-to-sobre").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("sobre").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
