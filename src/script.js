bed7976daeaf7849c184d17a7aa774e38cec81a4
Leonardo Mendonça <leonardomendonca@gmail.com>
2024-11-08 20:00:31 -0300

    ajuste na seção serviços

--- src/script.js
+++ src/script.js
@@ -29,3 +29,36 @@ document.getElementById("link-to-sobre").addEventListener("click", function(even
         behavior: "smooth" // Define rolagem suave
     })
 });
+
+//parte do rodapé
+//rolagem para inicio
+document.getElementById("link-to-inicio2").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("inicio").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
+
+//rolagem para serviços
+document.getElementById("link-to-servicos2").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("servicos").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
+
+//rolagem para contato
+document.getElementById("link-to-contato2").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("contato").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
+
+//rolagem para sobre
+document.getElementById("link-to-sobre2").addEventListener("click", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do link
+    document.getElementById("sobre").scrollIntoView({
+        behavior: "smooth" // Define rolagem suave
+    })
+});
